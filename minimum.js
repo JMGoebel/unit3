3abd8d76cc9782fefd17272f369f92d3524409d1
Jason Goebel <goebel.jason@gmail.com>
2018-01-28 12:53:20 -0600

    ADDS: minimum.js **COMPLETE**

---
+++ minimum.js
@@ -0,0 +1,7 @@
+"use strict";
+/* Write a function min that takes two arguments and returns their minimum. */
+
+let min = ( x, y ) => { return x < y ? x : y; };
+
+console.log( min(0, 10) );
+console.log( min(0, -10) );
